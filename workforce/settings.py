bb2d818eaf072e88f0d06133c08f1827844a82a7
YenchenLiu <mail@daychen.tw>
2025-07-14 13:45:39 +1000

    add mypy configuration and update dependencies for Django project

--- workforce/settings.py
+++ workforce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$!2#)@^n@r_p*oaro$r#q*smt&oy40g!de*4arbzlr8vj+e*7+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
